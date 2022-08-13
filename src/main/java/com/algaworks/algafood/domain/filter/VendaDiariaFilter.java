a61b0aa5d523e15811504a7905ea7ef60c7a2c87
Renato Reis Brasil <rbrasilrenato@gmail.com>
2022-08-13 15:42:20 -0300

    13.15. Desafio: adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/algaworks/algafood/domain/filter/VendaDiariaFilter.java
+++ src/main/java/com/algaworks/algafood/domain/filter/VendaDiariaFilter.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.domain.repository.filter;
+package com.algaworks.algafood.domain.filter;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +8,8 @@ import java.time.OffsetDateTime;
 
 @Setter
 @Getter
-public class PedidoFilter {
-    private Long clienteId;
+public class VendaDiariaFilter {
+
     private Long restauranteId;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
