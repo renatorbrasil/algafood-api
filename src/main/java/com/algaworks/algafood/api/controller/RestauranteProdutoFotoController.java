1cdcad0a62a4cf4efb77a8e7babd8d89f83a260a
Renato Reis Brasil <rbrasilrenato@gmail.com>
2022-08-14 22:45:17 -0300

    14.4. Desafio: Validando o content type do arquivo

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.dto.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId,
+                              @PathVariable Long produtoId,
+                              @Valid FotoProdutoInput fotoProdutoInput) {
+        var arquivo = fotoProdutoInput.getArquivo();
+        var nomeArquivo = UUID.randomUUID().toString() + "_" + arquivo.getOriginalFilename();
+        var pathArquivo = Path.of("/Users/renato", nomeArquivo);
+
+        System.out.println(pathArquivo);
+        System.out.println(arquivo.getContentType());
+
+        try {
+            arquivo.transferTo(pathArquivo);
+        } catch (IllegalStateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
