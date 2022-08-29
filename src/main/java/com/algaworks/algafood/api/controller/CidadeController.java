dcba3fe379511e199de82826d0b14bd698fc4fa0
renatorbrasil <renatorbrasil@gmail.com>
2022-08-29 20:05:01 -0300

    18.9. Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -8,6 +8,9 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
@@ -27,17 +31,20 @@ public class CidadeController {
 
 	@Autowired
 	private CidadeMapper cidadeMapper;
-	
+
+	@ApiOperation("Listar todas as cidades")
 	@GetMapping
 	public List<CidadeModel> listar() {
 		return cidadeMapper.map(cidadeRepository.findAll());
 	}
 
+	@ApiOperation("Buscar cidade por id")
 	@GetMapping("/{cidadeId}")
-	public CidadeModel buscar(@PathVariable Long cidadeId) {
+	public CidadeModel buscar(@PathVariable @ApiParam(value = "ID de uma cidade") Long cidadeId) {
 		return cidadeMapper.map(cadastroCidade.buscar(cidadeId));
 	}
 
+	@ApiOperation("Cadastrar uma nova cidade")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
@@ -49,8 +56,9 @@ public class CidadeController {
 		}
 	}
 
+	@ApiOperation("Atualizar uma cidade por ID")
 	@PutMapping("/{cidadeId}")
-	public CidadeModel atualizar(@PathVariable Long cidadeId,
+	public CidadeModel atualizar(@PathVariable @ApiParam("ID de uma cidade") Long cidadeId,
 							@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidadeAtual = cadastroCidade.buscar(cidadeId);
@@ -61,9 +69,10 @@ public class CidadeController {
 		}
 	}
 
+	@ApiOperation("Excluir uma cidade por ID")
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cidadeId) {
+	public void remover(@PathVariable @ApiParam("ID de uma cidade") Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
 	}
 
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,7 +2,11 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,6 +18,19 @@ public class SpringFoxConfig {
             .select()
                 .apis(RequestHandlerSelectors.basePackage(
                         "com.algaworks.algafood.api"))
+                .build()
+            .apiInfo(apiInfo())
+            .tags(new Tag("Cidades", "Gerencia as cidades"));
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("AlgaFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("AlgaWorks",
+                        "https://www.algaworks.com",
+                        "contato@algaworks.com"))
                 .build();
     }
 }
