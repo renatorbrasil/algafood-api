8c40de81b53bc94301b855b8879638e951311e31
renatorbrasil <renatorbrasil@gmail.com>
2022-08-31 21:31:48 -0300

    18.17. Desacoplando anotações do Swagger dos controladores

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,34 +1,24 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.controller.openapi.CidadeControllerOpenApi;
 import com.algaworks.algafood.api.dto.input.CidadeInput;
 import com.algaworks.algafood.api.dto.model.CidadeModel;
-import com.algaworks.algafood.api.exceptionHandler.ApiError;
 import com.algaworks.algafood.api.mapper.CidadeMapper;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Tag(name = "Cidades")
 @RestController
 @RequestMapping("/cidades")
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
@@ -39,28 +29,16 @@ public class CidadeController {
 	@Autowired
 	private CidadeMapper cidadeMapper;
 
-	@Operation(summary = "Listar todas as cidades")
 	@GetMapping
-	@ApiResponses(value = {
-			@ApiResponse(description = "Sucesso", responseCode = "200"),
-			@ApiResponse(description = "Requisição inválida (erro do cliente)", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
-	})
 	public List<CidadeModel> listar() {
 		return cidadeMapper.map(cidadeRepository.findAll());
 	}
 
-	@Operation(summary = "Buscar cidade por id")
 	@GetMapping("/{cidadeId}")
-	@ApiResponses(value = {
-			@ApiResponse(description = "Sucesso", responseCode = "200"),
-			@ApiResponse(description = "Cidade não encontrada", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))),
-			@ApiResponse(description = "Id da cidade inválido", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
-	})
-	public CidadeModel buscar(@PathVariable @Parameter(description = "ID de uma cidade") Long cidadeId) {
+	public CidadeModel buscar(@PathVariable Long cidadeId) {
 		return cidadeMapper.map(cadastroCidade.buscar(cidadeId));
 	}
 
-	@Operation(summary = "Cadastrar uma nova cidade")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
@@ -72,9 +50,8 @@ public class CidadeController {
 		}
 	}
 
-	@Operation(summary = "Atualizar uma cidade por ID")
 	@PutMapping("/{cidadeId}")
-	public CidadeModel atualizar(@PathVariable @Parameter(description = "ID de uma cidade") Long cidadeId,
+	public CidadeModel atualizar(@PathVariable Long cidadeId,
 							@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidadeAtual = cadastroCidade.buscar(cidadeId);
@@ -85,10 +62,9 @@ public class CidadeController {
 		}
 	}
 
-	@Operation(summary = "Excluir uma cidade por ID")
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable @Parameter(description = "ID de uma cidade") Long cidadeId) {
+	public void remover(@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
 	}
 
---
+++ src/main/java/com/algaworks/algafood/api/controller/openapi/CidadeControllerOpenApi.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood.api.controller.openapi;
+
+import com.algaworks.algafood.api.dto.input.CidadeInput;
+import com.algaworks.algafood.api.dto.model.CidadeModel;
+import com.algaworks.algafood.api.exceptionHandler.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+@Tag(name = "Cidades")
+public interface CidadeControllerOpenApi {
+
+    @Operation(summary = "Listar todas as cidades")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Sucesso", responseCode = "200"),
+            @ApiResponse(description = "Requisição inválida (erro do cliente)", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
+    List<CidadeModel> listar();
+
+    @Operation(summary = "Buscar cidade por id")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Sucesso", responseCode = "200"),
+            @ApiResponse(description = "Cidade não encontrada", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))),
+            @ApiResponse(description = "Id da cidade inválido", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class)))
+    })
+    CidadeModel buscar(@Parameter(description = "ID de uma cidade") Long cidadeId);
+
+    @Operation(summary = "Cadastrar uma nova cidade")
+    CidadeModel adicionar(CidadeInput cidadeInput);
+
+    @Operation(summary = "Atualizar uma cidade por ID")
+    CidadeModel atualizar(@Parameter(description = "ID de uma cidade") Long cidadeId,
+                                 CidadeInput cidadeInput);
+
+    @Operation(summary = "Excluir uma cidade por ID")
+    void remover(@Parameter(description = "ID de uma cidade") Long cidadeId);
+
+}
