b6362912a4c178603aefd261bc8ff52914f92817
renatorbrasil <renatorbrasil@gmail.com>
2022-10-22 16:36:53 -0300

    23.14. Autenticando usuário com dados do banco de dados

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,13 +1,8 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
@@ -19,13 +14,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
             .and().cors()
             .and().oauth2ResourceServer().jwt();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        var secretKey = new SecretKeySpec("fdafadsfadsfaf11dfasdfadsfasdfasdfasdfadsfasdfasdfasdfsadfasdfsdafasfdsafa".getBytes(),
-                "HmacSHA256");
-        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
